6ebdf5c47e55b42d3234a6a9080b3b2a3ab20423
bahrmichael <mbahr92@gmail.com>
2017-01-15 21:40:46 +0100

    added codeship and limited coverage to unit tests

--- unit_test_coverage.sh
+++ unit_test_coverage.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # clone into a new folder, as git will not allow the current folder as it is not empty
 git clone https://github.com/bahrmichael/aws-python-stack repo
-cd repo
+cd repo/tests/unit
 coverage run -m unittest discover
 COVERALLS_REPO_TOKEN=$TOKEN coveralls
