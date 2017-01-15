82457544b37e08182bd5e36f7400b3d51f34b20e
bahrmichael <mbahr92@gmail.com>
2017-01-15 21:27:14 +0100

    updated coverage file to my repo + added build step with coverage dependencies

--- test_coverage.sh
+++ test_coverage.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # clone into a new folder, as git will not allow the current folder as it is not empty
-git clone https://github.com/<USER>/<REPO> <FOLDER>
-cd <FOLDER>
+git clone https://github.com/bahrmichael/aws-python-stack repo
+cd repo
 coverage run -m unittest discover
 COVERALLS_REPO_TOKEN=$TOKEN coveralls
