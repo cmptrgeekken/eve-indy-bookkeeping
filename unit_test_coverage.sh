d87b1193b7ea54e26c4694cc79f308dc618ee42b
bahrmichael <mbahr92@gmail.com>
2017-01-15 22:07:51 +0100

    added coveralls token and fixed coveralls reporting

--- unit_test_coverage.sh
+++ unit_test_coverage.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
+# we need the repo for coveralls' reports
 # clone into a new folder, as git will not allow the current folder as it is not empty
 git clone https://github.com/bahrmichael/aws-python-stack repo
-cd repo/tests/unit
+mv repo/.git .
+rm -Rf repo
 coverage run -m unittest discover
 COVERALLS_REPO_TOKEN=$TOKEN coveralls
